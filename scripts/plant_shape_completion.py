03473e64506e5df03007717e1360c85e283bbb10
Shaoxiong Yao <syao16@illinois.edu>
2025-04-20 16:42:00 -0500

    update shape complete namings

--- scripts/plant_shape_completion.py
+++ scripts/plant_shape_completion.py
@@ -40,8 +40,8 @@ from data.arm_configs import home_angles, qlimits, home_pose
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--asset_dir', type=str, default='out_data/plant_assets')
-    parser.add_argument('--obj_name', type=str, default='test_obj_1714573815')
+    parser.add_argument('--segment_dir', type=str, help='plant segmentation directory, should contains fruit, leaf, branch pcd')
+    parser.add_argument('--shape_complete_dir', type=str, help='plant shape completion output directory')
     parser.add_argument('--trans_params_fn', type=str, default='data/tripod_finetune_params1.json')
     parser.add_argument('--voxel_size', type=float, default=0.003)
     parser.add_argument('--free_space_layer', type=int, default=1)
@@ -68,17 +68,20 @@ if __name__ == '__main__':
     cam2rob_trans = np.array(camera_params['cam2rob'])
     cam_center = cam2rob_trans[:3, 3]
 
-    obj_data_dir = Path(f'{args.asset_dir}/{args.obj_name}')
-    branch_pcd = o3d.io.read_point_cloud(str(obj_data_dir / f'{CLASSES[0]}_pcd.ply'))
-    leaf_pcd = o3d.io.read_point_cloud(str(obj_data_dir / f'{CLASSES[1]}_pcd.ply'))
-    fruit_pcd = o3d.io.read_point_cloud(str(obj_data_dir / f'{CLASSES[2]}_pcd.ply'))
-    # o3d.visualization.draw_geometries_with_editing([leaf_pcd])
+    # Prepare input and output directories
+    segment_dir = Path(args.segment_dir)
+    shape_complete_dir = Path(args.shape_complete_dir)
+    shape_complete_dir.mkdir(parents=True, exist_ok=True)
+
+    branch_pcd = o3d.io.read_point_cloud(str(segment_dir / f'{CLASSES[0]}_pcd.ply'))
+    leaf_pcd = o3d.io.read_point_cloud(str(segment_dir / f'{CLASSES[1]}_pcd.ply'))
+    fruit_pcd = o3d.io.read_point_cloud(str(segment_dir / f'{CLASSES[2]}_pcd.ply'))
+    cropped_scene_pcd = o3d.io.read_point_cloud(str(segment_dir / f'cropped_scene_pcd.ply'))
 
     # Preprocess leaf and fruit point clouds
-    leaf_pcd = get_largest_dbscan_component(leaf_pcd, nn_radius=args.nn_radius, min_points=30, vis_pcd=True)
-    fruit_pcd = get_largest_dbscan_component(fruit_pcd, nn_radius=args.nn_radius, min_points=30, vis_pcd=True)
+    leaf_pcd = get_largest_dbscan_component(leaf_pcd, nn_radius=args.nn_radius, min_points=30, vis_pcd=False)
+    fruit_pcd = get_largest_dbscan_component(fruit_pcd, nn_radius=args.nn_radius, min_points=30, vis_pcd=False)
 
-    raw_scene_pcd = o3d.io.read_point_cloud(str(obj_data_dir / f'crop_space_merge_pcd.ply'))
     
     fruit_pts = np.array(fruit_pcd.points)
     leaf_pts = np.array(leaf_pcd.points)
@@ -94,8 +97,7 @@ if __name__ == '__main__':
     end_time = time.time()
     print('Time taken:', end_time - start_time)
     time_stats_dict['branch_completion_time'] = end_time - start_time
-    o3d.io.write_triangle_mesh(str(obj_data_dir / 'completed_branch_mesh.ply'), completed_branch_mesh)
-    o3d.io.write_point_cloud(str(obj_data_dir / 'completed_branch_pcd.ply'), completed_branch_pcd)
+    o3d.io.write_triangle_mesh(str(shape_complete_dir / 'completed_branch_mesh.ply'), completed_branch_mesh)
 
     simplified_completed_branch_mesh = completed_branch_mesh.simplify_quadric_decimation(300)
     o3d.visualization.draw_geometries([completed_branch_pcd, branch_pcd, leaf_pcd, fruit_pcd, completed_branch_mesh])
@@ -103,14 +105,14 @@ if __name__ == '__main__':
     
     """Compute free space points using octomap"""
     octomap_wrapper = OctomapWrapper()
-    free_space_pts = octomap_wrapper.compute_free_space_points(raw_scene_pcd, fruit_pts, cam_center,
+    free_space_pts = octomap_wrapper.compute_free_space_points(cropped_scene_pcd, fruit_pts, cam_center,
                                                                args.voxel_size, args.fruit_max_size,
                                                                args.free_space_bbx_scale, args.free_space_layer)
     free_space_pcd = o3d.geometry.PointCloud()
     free_space_pcd.points = o3d.utility.Vector3dVector(free_space_pts)
     
     o3d.visualization.draw_geometries([free_space_pcd, fruit_pcd, branch_pcd, leaf_pcd], window_name='Free Space PCD')
-    o3d.io.write_point_cloud(str(obj_data_dir / 'free_space_pcd.ply'), free_space_pcd)
+    o3d.io.write_point_cloud(str(shape_complete_dir / 'free_space_pcd.ply'), free_space_pcd)
     """End of compute free space points using octomap"""
 
     
@@ -135,16 +137,16 @@ if __name__ == '__main__':
                                               lr=args.lr, output_mesh_res=args.output_mesh_res, verbose=True, vis=True)
     fit_fruit_mesh = out_dict['final_shape_mesh']
     time_stats_dict['fit_time'] = out_dict['time']
-    o3d.io.write_triangle_mesh(str(obj_data_dir / 'fit_fruit_mesh.ply'), fit_fruit_mesh)
+    o3d.io.write_triangle_mesh(str(shape_complete_dir / 'completed_fruit_mesh.ply'), fit_fruit_mesh)
     """End of scene-consistent shape completion of fruit"""
 
     # save the shape completion results
-    with open(obj_data_dir / 'fit_params.json', 'w') as f:
-        fit_params = {
+    with open(shape_complete_dir / 'completed_fruit_sim3_params.json', 'w') as f:
+        sim3_params = {
             'translation': out_dict['translation'].tolist(),
             'rotation': out_dict['rotation'].tolist(),
             'scale': out_dict['scale'].tolist()   
         }
-        json.dump(fit_params, f, indent=2)
-    torch.save(out_dict['latent'], f'{obj_data_dir}/fit_latent.pth')
+        json.dump(sim3_params, f, indent=2)
+    torch.save(out_dict['latent'], shape_complete_dir / 'completed_fruit_latent.pth' )
     
